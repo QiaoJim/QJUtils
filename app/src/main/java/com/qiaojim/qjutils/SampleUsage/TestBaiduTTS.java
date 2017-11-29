1f308c852f0ff411a562e9e4c2199a13175e0e00
QiaoJim <919106848@qq.com>
2017-11-29 20:09:54 +0800

    完善TTS封装工具

--- app/src/main/java/com/qiaojim/qjutils/SampleUsage/TestBaiduTTS.java
+++ app/src/main/java/com/qiaojim/qjutils/SampleUsage/TestBaiduTTS.java
@@ -3,35 +3,25 @@ package com.qiaojim.qjutils.SampleUsage;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.baidu.tts.client.SpeechSynthesizer;
-import com.baidu.tts.client.SpeechSynthesizerListener;
 import com.baidu.tts.client.TtsMode;
-import com.qiaojim.qjutils.BaiduTTS.InitConfig;
 import com.qiaojim.qjutils.BaiduTTS.NonBlockSyntherizer;
 import com.qiaojim.qjutils.BaiduTTS.OfflineResource;
 import com.qiaojim.qjutils.BaiduTTS.QJBaiduTtsUtils;
 import com.qiaojim.qjutils.BaiduTTS.QJTtsInitConfig;
-import com.qiaojim.qjutils.BaiduTTS.UiMessageListener;
 import com.qiaojim.qjutils.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
-
-import static com.qiaojim.qjutils.BaiduTTS.MainHandlerConstant.PRINT;
-import static com.qiaojim.qjutils.BaiduTTS.MainHandlerConstant.UI_CHANGE_INPUT_TEXT_SELECTION;
-import static com.qiaojim.qjutils.BaiduTTS.MainHandlerConstant.UI_CHANGE_SYNTHES_TEXT_SELECTION;
 
 public class TestBaiduTTS extends AppCompatActivity {
 
@@ -40,24 +30,11 @@ public class TestBaiduTTS extends AppCompatActivity {
 
     private NonBlockSyntherizer speechSynthesizer;
 
-    // ================选择TtsMode.ONLINE  不需要设置以下参数; 选择TtsMode.MIX 需要设置下面2个离线资源文件的路径
-    private static String TEMP_DIR = "/sdcard/baiduTTS"; //重要！请手动将assets目录下的3个dat 文件复制到该目录
-
-    private static String TEXT_FILENAME = TEMP_DIR + "/" + "bd_etts_text.dat"; // 请确保该PATH下有这个文件
-    private static String MODEL_FILENAME = TEMP_DIR + "/" + "bd_etts_speech_male.dat"; // 请确保该PATH下有这个文件 male是男声 female女声
-
-    private Handler mainHandler;
-
     protected String appId = "10457134";
-
     protected String appKey = "TeUEBcZNsLy2mUT9XfWUdVuw";
-
     protected String secretKey = "3GQqnQKa47G7nGuteQ6ldNhP4oWobnZd ";
-
-
     // TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； 没有纯离线
     protected TtsMode ttsMode = TtsMode.MIX;
-
     // 离线发音选择，VOICE_FEMALE即为离线女声发音。
     // assets目录下bd_etts_speech_female.data为离线男声模型；bd_etts_speech_female.data为离线女声模型
     protected String offlineVoice = OfflineResource.VOICE_FEMALE;
@@ -65,46 +42,42 @@ public class TestBaiduTTS extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_qjtts);
+        setContentView(R.layout.activity_test_tts);
 
         initPermission();
         initView();
 
-        initialTts(); // 初始化TTS引擎
+        // 初始化TTS引擎，封装工具使用示例
+        initialTts();
     }
 
+    /**
+     * QJBaiduTtsUtils 封装工具使用
+     */
     protected void initialTts() {
+        /*设置授权信息，注册应用服务*/
         QJTtsInitConfig config = new QJTtsInitConfig();
         config.setApiKey(appKey);
         config.setAppId(appId);
         config.setOfflineVoice(offlineVoice);
         config.setSecretKey(secretKey);
         config.setTtsMode(ttsMode);
-        speechSynthesizer = QJBaiduTtsUtils.initTts(this, config);
-//        // 设置初始化参数
-//        SpeechSynthesizerListener listener = new UiMessageListener(new Handler()); // 此处可以改为 含有您业务逻辑的SpeechSynthesizerListener的实现类
-//
-//        Map<String, String> params = getParams();
-//
-//        // appId appKey secretKey 网站上您申请的应用获取。注意使用离线合成功能的话，需要应用中填写您app的包名。包名在build.gradle中获取。
-//        InitConfig initConfig = new InitConfig(appId, appKey, secretKey, ttsMode, offlineVoice, params, listener);
-//
-//        speechSynthesizer = new NonBlockSyntherizer(this, initConfig, mainHandler); // 此处可以改为MySyntherizer 了解调用过程
-    }
 
-    protected Map<String, String> getParams() {
+        //获取默认实例或自定义实例，2种调用方法
+
+        /*获取默认实例，调用其speak（str）方法，即可播报str
+        speechSynthesizer = QJBaiduTtsUtils.initTts(this, config);*/
+
+        /*获取自定义实例*/
         Map<String, String> params = new HashMap<String, String>();
-        // 以下参数均为选填
-        params.put(SpeechSynthesizer.PARAM_SPEAKER, "0"); // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
-        params.put(SpeechSynthesizer.PARAM_VOLUME, "5"); // 设置合成的音量，0-9 ，默认 5
-        params.put(SpeechSynthesizer.PARAM_SPEED, "5");// 设置合成的语速，0-9 ，默认 5
-        params.put(SpeechSynthesizer.PARAM_PITCH, "5");// 设置合成的语调，0-9 ，默认 5
-        params.put(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);         // 该参数设置为TtsMode.MIX生效。即纯在线模式不生效。
-        // MIX_MODE_DEFAULT 默认 ，wifi状态下使用在线，非wifi离线。在线状态下，请求超时6s自动转离线
-        // MIX_MODE_HIGH_SPEED_SYNTHESIZE_WIFI wifi状态下使用在线，非wifi离线。在线状态下， 请求超时1.2s自动转离线
-        // MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
-        // MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
-        return params;
+        // 更多自定义参数还是参考官网吧
+        params.put(SpeechSynthesizer.PARAM_SPEAKER, "0");   // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
+        params.put(SpeechSynthesizer.PARAM_VOLUME, "5");    // 设置合成的音量，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_SPEED, "5");     // 设置合成的语速，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_PITCH, "5");     // 设置合成的语调，0-9 ，默认 5
+        params.put(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);         // 该参数设置为TtsMode
+        speechSynthesizer = QJBaiduTtsUtils.initTts(this, config);
+
     }
 
     private void initView() {
@@ -113,15 +86,14 @@ public class TestBaiduTTS extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*语音播报输入框内容*/
                 String text = editText.getText().toString();
                 speechSynthesizer.speak(text);
             }
         });
     }
 
-    /**
-     * android 6.0 以上需要动态申请权限
-     */
+    /*android 6.0 以上需要动态申请权限*/
     private void initPermission() {
         String permissions[] = {
                 Manifest.permission.INTERNET,
